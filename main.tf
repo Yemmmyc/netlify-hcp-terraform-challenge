ba835c50a3aca3fd735a34e2237c6399ba94b2b6
Yemmmyc <yemmmyc@hotmail.com>
2025-08-20 02:17:45 +0100

    Updated main.tf and .gitignore

--- main.tf
+++ main.tf
@@ -1,24 +1,22 @@
-terraform {
-  required_providers {
-    netlify = {
-      source  = "netlify/netlify"
-      version = "~> 1.0"   # upgrade to 1.x
-    }
-  }
-
-  backend "remote" {
-    organization = "Yemmmyc"
-
-    workspaces {
-      name = "netlify-hcp-terraform-challenge"
-    }
-  }
-}
-
 provider "netlify" {
   token = var.netlify_token
 }
 
-resource "netlify_site" "example" {
+data "netlify_site" "example" {
   name = "netlify-terraform-starter"
+}
+
+# Example environment variables
+resource "netlify_environment_variable" "example1" {
+  site_id = "b522540e-35de-4167-839b-1b8e921e75c9"
+  key     = "TEST_VARIABLE"
+  value   = "HelloWorld"
+  scope   = "build"
+}
+
+resource "netlify_environment_variable" "example2" {
+  site_id = "b522540e-35de-4167-839b-1b8e921e75c9"
+  key     = "API_ENDPOINT"
+  value   = "https://api.example.com"
+  scope   = "runtime"
 }
