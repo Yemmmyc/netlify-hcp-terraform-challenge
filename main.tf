e4e3a02d685c1544448f23d923a8782f49c45280
Yemmmyc <yemmmyc@hotmail.com>
2025-08-20 02:07:02 +0100

    Updated main.tf and .gitignore

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 terraform {
+  required_providers {
+    netlify = {
+      source  = "netlify/netlify"
+      version = "~> 1.0"   # upgrade to 1.x
+    }
+  }
+
   backend "remote" {
     organization = "Yemmmyc"
 
     workspaces {
       name = "netlify-hcp-terraform-challenge"
-    }
-  }
-
-  required_providers {
-    netlify = {
-      source  = "netlify/netlify"
-      version = "~> 0.2"
     }
   }
 }
